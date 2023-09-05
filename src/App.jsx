0261a9efdef9f635dab8cf78f91b321e9b4fecaa
aydinschwa <aydinschwa@gmail.com>
2023-09-05 00:40:33 -0700

    finish bare bones implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,126 @@ import React from 'react'
 import Intro from "./Intro"
 import Question from "./Question"
 import { nanoid } from 'nanoid'
+import { decode } from "html-entities";
 
 function App() {
   const [started, setStarted] = React.useState(false)
     const [guessesSubmitted, setGuessesSubmitted] = React.useState(false)
+    const [score, setScore] = React.useState(0)
     const [questionData, setQuestionData] = React.useState([])
+    const [triviaQuestions, setTriviaQuestions] = React.useState([])
+    const [reset, setReset] = React.useState(0)
 
+    // get trivia questions from API
     React.useEffect(() => { 
         fetch("https://opentdb.com/api.php?amount=5")
         .then(response => response.json())
         .then(response => setQuestionData(response.results))
-    }, [])
-    
-    const triviaQuestionsElements = questionData.map(question => {
+    }, [reset])
+
+    // grab question and answer from questions
+    const createAnswers = (correctAnswer, incorrectAnswers) => {
+
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }   
+        const answerChoices = []
+        for (const incorrectAnswer of incorrectAnswers) {
+            answerChoices.push({answer: decode(incorrectAnswer), 
+                                isCorrect: false,
+                                answerId: nanoid(), 
+                                selected: false})
+        }
+        answerChoices.push({answer: decode(correctAnswer),
+                            isCorrect: true,
+                            answerId: nanoid(),
+                            selected: false})
+
+        // randomly shuffle choices
+        return shuffleArray(answerChoices)
+    }
+
+    // set trivia question state whenever question data changes
+    React.useEffect(() => {
+      const questionAnswers = []
+      for (let question of questionData) {
+        const answers = createAnswers(question.correct_answer, question.incorrect_answers)
+        questionAnswers.push({
+          question: decode(question.question),
+          answers: answers,
+          questionId: nanoid()
+        })
+      }
+      setTriviaQuestions(Array.from(questionAnswers))
+    }, [questionData])
+
+    const toggleAnswer = (questionId, answerId)=> {
+      setTriviaQuestions(oldTriviaQuestions => {
+        const updatedTriviaQuestions = [...oldTriviaQuestions]
+        for (let question of updatedTriviaQuestions) {
+          if (questionId !== question.questionId) { continue }
+          for (let answer of question.answers) {
+            if (answer.answerId === answerId) {
+              answer.selected = true;
+            }
+            else {
+              answer.selected = false;
+            }
+          }
+        }
+      return updatedTriviaQuestions
+      })
+    } 
+
+    const submitAnswers = () => {
+      let score = 0
+      for (let question of triviaQuestions) {
+        for (let answer of question.answers) {
+          if (answer.isCorrect && answer.selected) {
+            score += 1
+          }
+        }
+      }
+      setScore(score)
+      setGuessesSubmitted(true) 
+    }
+
+    const triviaQuestionsElements = triviaQuestions.map(question => {
         return ( 
-            <Question items={{...question}} key={nanoid()}/>
+            <Question 
+              question={question.question}
+              answers={question.answers}
+              questionId={question.questionId}
+              toggle={toggleAnswer}
+              gameOver={guessesSubmitted}
+              key={nanoid()}/>
         )})
-    
+
+      const resetGame = () => {
+        setReset(oldReset => oldReset + 1)
+        setGuessesSubmitted(false)
+      }
+
     return ( 
         <div>
             {started ? triviaQuestionsElements 
                      : <Intro toggleStart={() => setStarted(true)}/>}
-            {started && <button className="checkAnswersButton"
-                    onClick={() => console.log('fix me')}
-            >Check Answers</button>}
+            {started && 
+             (guessesSubmitted ? 
+                      (<div className="gameOverText">
+                       <p className="scoreText">{`You scored ${score}/5 correct answers`}</p>
+                       <button className="playAgainButton"
+                               onClick={resetGame}>Play again</button>
+                       </div>)
+                              : 
+                      (<button className="checkAnswersButton"
+                                onClick={submitAnswers}
+                        >Check Answers</button>))
+            }
         </div>
     )
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,27 @@
 
 
 export default function Button(props) {
-    const {answer, isCorrect, id, selected, toggle} = props
-    const styles = selected ? {backgroundColor: "#94D7A2"}: {}
+    const {answer, questionId, answerId, selected, isCorrect, gameOver, toggle} = props
+
+    let styles = {}
+    if (!gameOver && selected) {
+        styles = {backgroundColor: "#D6DBF5"}
+    }
+    else if (gameOver) {
+        if (isCorrect) {
+            styles =  {backgroundColor: "#94D7A2"}
+        }
+        else if (selected && !isCorrect) {
+            styles = {backgroundColor: "#F8BCBC", opacity: 0.5}
+        }
+        else {
+            styles = {opacity: 0.5}
+        }
+    }
+    
     return (
         <button
          style={styles}
-         onClick={() => toggle(id)}>{answer}</button>
+         onClick={() => toggle(questionId, answerId)}>{answer}</button>
     )
 }
